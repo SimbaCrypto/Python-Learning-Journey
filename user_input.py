31a9450a2fa4e68065d70bf295d26d2f7b1a0313
Simbacrypto <simbacryptocurrency@gmail.com>
2024-11-01 16:41:16 +0100

    Initial commit

---
+++ user_input.py
@@ -0,0 +1,16 @@
+# Asking user for their name, age, and location
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+
+name = "Michael"
+age = 30
+location = "Ghana"
+
+print(name)
+print(age)
+print(location)
+
+# Print a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
+
