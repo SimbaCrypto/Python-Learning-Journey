e57d141e7514577334a976c697a41f1e166ca461
Simbacrypto <simbacryptocurrency@gmail.com>
2024-11-10 12:51:12 +0100

    Python Week 2 Assignment

---
+++ my_list.py
@@ -0,0 +1,28 @@
+# Creating an empty list called my_list.
+my_list = []
+
+# Appending the elements 10, 20, 30, 40 to my_list and print the result.
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+
+# Inserting the value 15 at the second position in the list and print the result.
+my_list.insert(1, 15)
+print(my_list)
+
+# Extending my_list with another list: [50, 60, 70] and print the result.
+my_list.extend([50, 60, 70])
+print(my_list)
+
+# Removin the last element from my_list and print the result.
+my_list.pop()
+print(my_list)
+
+# Sorting my_list in ascending order and print the result.
+my_list.sort()
+print(my_list)
+
+# Finding and print the index of the value 30 in my_list.
+print(my_list.index(30))
